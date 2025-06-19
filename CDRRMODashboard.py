b80f0776de44c5fe3e6321eca55308d15fccb74a
Kumitokiru <161684032+Kumitokiru@users.noreply.github.com>
2025-06-19 17:37:59 +0800

    Delete CDRRMODashboard.py

--- CDRRMODashboard.py
+++
@@ -1,6 +0,0 @@
-from alert_data import alerts
-from collections import Counter
-
-def get_cdrmo_stats():
-    types = [a.get('emergency_type', 'unknown') for a in alerts if a.get('role') == 'cdrmo' or a.get('municipality')]
-    return Counter(types)
